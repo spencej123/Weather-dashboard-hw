619cf3a8b80598427cac36e538ee3a3a00c2826c
Spencer Johnson <spencer.johnson123@hotmail.com>
2020-11-16 22:49:03 -0600

    working on the .append() method

--- script.js
+++ script.js
@@ -27,11 +27,29 @@ function getWeather(cityName) {
     // append the results to the page
     // JQuery has a .append() method
     console.log(result);
-    $("#weather-result").append(`<div id="result">
-    <h1>${result.name}</h1>
-    <p></p>
-  </div>`);
-  });
+    let currentDate = new Date(response.data.dt*1000);
+            console.log(currentDate);
+            let day = currentDate.getDate();
+            let month = currentDate.getMonth() + 1;
+            let year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt",response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+        let lat = response.data.coord.lat;
+        let lon = response.data.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+        axios.get(UVQueryURL)
+        .then(function(response){
+            let UVIndex = document.createElement("span");
+            UVIndex.setAttribute("class","badge badge-danger");
+            UVIndex.innerHTML = response.data[0].value;
+            currentUVEl.innerHTML = "UV Index: ";
+            currentUVEl.append(UVIndex);
+        });
 }
 
 $(document).ready(() => {
